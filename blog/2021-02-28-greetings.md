4c9c7d7bce739f74f24ccea43039d693c4478ec4
sebastienlorber <lorber.sebastien@gmail.com>
2022-10-14 12:13:15 +0200

    test

---
+++ blog/2021-02-28-greetings.md
@@ -0,0 +1,11 @@
+---
+slug: /test/bluehats
+title: Greetings!
+authors: [slorber, endi, yangshun]
+tags: [bluehats]
+---
+
+Congratulations, you have made your first post!
+
+Feel free to play around and edit this post as much you like.
+
---
+++ src/highlight.js
@@ -0,0 +1,17 @@
+
+
+export default function Highlight ({children, color}) (
+  <span
+    style={{
+      backgroundColor: color,
+      borderRadius: '20px',
+      color: '#fff',
+      padding: '10px',
+      cursor: 'pointer',
+    }}
+    onClick={() => {
+      alert(`You clicked the color ${color} with label ${children}`)
+    }}>
+    {children}
+  </span>
+);
